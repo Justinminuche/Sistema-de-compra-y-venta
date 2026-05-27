6281a5c765e0c348110a0ad34701265de69c1387
Justin Minuche <jminucheg@unemi.edu.ec>
2026-05-27 17:59:36 -0500

    security: add csrf trusted origins for production

--- proy_vbc/settings.py
+++ proy_vbc/settings.py
@@ -162,3 +162,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://sistema-de-compra-y-venta.onrender.com']
